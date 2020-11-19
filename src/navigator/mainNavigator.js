0dc9f4eb527594993787986091196de4c854b1a5
Crowdbotics <team@crowdbotics.com>
2020-11-19 16:46:33 +0000

    add package blueprint-react-native-achievement-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Achievements2175933Navigator from '../features/Achievements2175933/navigator';
 import Achievements3175931Navigator from '../features/Achievements3175931/navigator';
 import Achievements4175930Navigator from '../features/Achievements4175930/navigator';
 import CalendarView5175929Navigator from '../features/CalendarView5175929/navigator';
@@ -16,6 +17,7 @@ import CalendarView5175929Navigator from '../features/CalendarView5175929/naviga
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Achievements2175933: { screen: Achievements2175933Navigator },
 Achievements3175931: { screen: Achievements3175931Navigator },
 Achievements4175930: { screen: Achievements4175930Navigator },
 CalendarView5175929: { screen: CalendarView5175929Navigator },
