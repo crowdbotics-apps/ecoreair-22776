b9868a684896109f28936ce7bb9fa3e6fcf5ca83
Crowdbotics <team@crowdbotics.com>
2020-11-19 16:41:13 +0000

    add package blueprint-react-native-achievement-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Achievements3175931', human_name: 'Achievements 3', access_route: 'Achievements3175931'},
 { name: 'Achievements4175930', human_name: 'Achievements 4', access_route: 'Achievements4175930'},
 { name: 'CalendarView5175929', human_name: 'Calendar View 5', access_route: 'CalendarView5175929', icon: 'calendar'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Achievements3175931Navigator from '../features/Achievements3175931/navigator';
 import Achievements4175930Navigator from '../features/Achievements4175930/navigator';
 import CalendarView5175929Navigator from '../features/CalendarView5175929/navigator';
 
@@ -15,6 +16,7 @@ import CalendarView5175929Navigator from '../features/CalendarView5175929/naviga
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Achievements3175931: { screen: Achievements3175931Navigator },
 Achievements4175930: { screen: Achievements4175930Navigator },
 CalendarView5175929: { screen: CalendarView5175929Navigator },
 
